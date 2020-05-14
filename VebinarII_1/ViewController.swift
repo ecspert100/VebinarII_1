b7a484dd34412f3e7f4a7fe5fce54d52a6b1390c
Alexey Burer <ecspert.07@gmail.com>
2020-05-14 16:03:59 +0300

    StartButton action, add outlets

--- VebinarII_1/ViewController.swift
+++ VebinarII_1/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
-
+    
+    @IBAction func startButtonPressed() {
+        
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            startButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            startButton.setTitle("ShowText", for: .normal)
+        }
+        
+    }
+    
 
 }
 
